f37bbbf7516ec9a15bb0b777c977b1f1530d5d0f
saltukhov <is1ntc@gmail.com>
2019-12-20 17:13:11 +0200

    Добавлена форма для добавления данных

--- src/main/java/is/ntc/simpleMVCweb/SimpleController.java
+++ src/main/java/is/ntc/simpleMVCweb/SimpleController.java
@@ -3,7 +3,10 @@ package is.ntc.simpleMVCweb;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SimpleController {
@@ -31,4 +34,21 @@ public class SimpleController {
 		return "book";
 	}
 	
+	@GetMapping("/addbook")
+	public String addbook(
+			@RequestParam(name="id", required=false, defaultValue="1") int id,
+			@RequestParam(name="name", required=false, defaultValue="") String name, 
+			@RequestParam(name="author", required=false, defaultValue="") String author,
+			@RequestParam(name="publisher", required=false, defaultValue="") String publisher,
+			@RequestParam(name="year", required=false, defaultValue="2000") int year,
+			Model model) {
+		model.addAttribute("book", new Book(id, name, author, publisher, year));
+		return "addbook";
+	}
+	
+	@PostMapping("/addbook")
+	  public String addbookSubmit(@ModelAttribute Book book) {
+	    return "newbook";
+	}
+
 }
